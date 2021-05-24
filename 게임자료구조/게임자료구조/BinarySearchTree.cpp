a0eea0bfb08dcfb1b6b7a8480f600c316e2efe54
oMFDOo <jinjoo021@naver.com>
2021-05-24 14:13:37 +0900

    이거 뭐징

--- 게임자료구조/게임자료구조/BinarySearchTree.cpp
+++ 게임자료구조/게임자료구조/BinarySearchTree.cpp
@@ -1,45 +1,45 @@
-//#include "BinarySearchTree.h"
-//
-///* 생성 트리
-// 4 - 9 - 3 - 7 - 6 - 9 - 2 - 12 - 11 - 13
-//       4
-//	  / \
-//	 3	 9
-//	/   / \
-//   2   7  12
-//      /  /  \
-//	 6	11  13
-//*/
-//int main() {
-//	BinarySearchTree<int> *BST = new BinarySearchTree<int>;
-//
-//	// 중복 데이터 9 존재
-//	int insertValue[] = { 4, 9, 3, 7, 6, 9, 2, 12, 11, 13};
-//	for (int i = 0; i < sizeof(insertValue) / sizeof(int); i++) {
-//		cout << "Insert Data : " << insertValue[i] << "   \t";
-//		if (BST->insertData(insertValue[i])) cout << "Succeed" << endl;
-//		else cout << "Fail" << endl;
-//	}
-//	BST->levelOrderPrint();
-//	cout << "\n\n\n";
-//
-//
-//	int find[] = { 1, 2, 3};
-//	for (int i = 0; i < sizeof(find) / sizeof(int); i++) {
-//		cout << "Data \'" << find[i] << "\' exist : ";
-//		if (BST->searchData(find[i])) cout << "True" << endl;
-//		else cout << "Fail" << endl;
-//	}
-//
-//	cout << "\n\n";
-//
-//	// 중복 삭제 데이터 4 존재
-//	int remove[] = { 4, 2, 9, 4 };
-//	for (int i = 0; i < sizeof(remove) / sizeof(int); i++) {
-//		cout << "Data \'" << remove[i] << "\' delete : ";
-//		if (BST->removeData(remove[i])) cout << "Succeed" << endl;
-//		else cout << "Fail" << endl;
-//		BST->levelOrderPrint();
-//		cout << "\n\n";
-//	}
-//}
+#include "BinarySearchTree.h"
+
+/* 생성 트리
+ 4 - 9 - 3 - 7 - 6 - 9 - 2 - 12 - 11 - 13
+       4
+	  / \
+	 3	 9
+	/   / \
+   2   7  12
+      /  /  \
+	 6	11  13
+*/
+int main() {
+	BinarySearchTree<int> *BST = new BinarySearchTree<int>;
+
+	// 중복 데이터 9 존재
+	int insertValue[] = { 4, 9, 3, 7, 6, 9, 2, 12, 11, 13};
+	for (int i = 0; i < sizeof(insertValue) / sizeof(int); i++) {
+		cout << "Insert Data : " << insertValue[i] << "   \t";
+		if (BST->insertData(insertValue[i])) cout << "Succeed" << endl;
+		else cout << "Fail" << endl;
+	}
+	BST->levelOrderPrint();
+	cout << "\n\n\n";
+
+
+	int find[] = { 1, 2, 3};
+	for (int i = 0; i < sizeof(find) / sizeof(int); i++) {
+		cout << "Data \'" << find[i] << "\' exist : ";
+		if (BST->searchData(find[i])) cout << "True" << endl;
+		else cout << "Fail" << endl;
+	}
+
+	cout << "\n\n";
+
+	// 중복 삭제 데이터 4 존재
+	int remove[] = { 4, 2, 9, 4 };
+	for (int i = 0; i < sizeof(remove) / sizeof(int); i++) {
+		cout << "Data \'" << remove[i] << "\' delete : ";
+		if (BST->removeData(remove[i])) cout << "Succeed" << endl;
+		else cout << "Fail" << endl;
+		BST->levelOrderPrint();
+		cout << "\n\n";
+	}
+}
--- 게임자료구조/게임자료구조/소스.cpp
+++ 게임자료구조/게임자료구조/소스.cpp
@@ -1,113 +1,113 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class phoneNode {
-public:
-    phoneNode(string name, string phonrNumber, string address, phoneNode* next = 0);
-    void printPD();
-    phoneNode* next_;
-    string name_;
-    string phoneNumber_;
-    string address_;
-};
-
-class phoneBook {
-public:
-    phoneBook();
-    bool Insert(string name, string phonrNumber, string address);
-    void Delete(string name);
-    phoneNode* Find(string name);
-private:
-    phoneNode* lst_;
-};
-phoneBook::phoneBook()
-{
-    phoneNode* lst_ = NULL;
-};
-phoneNode::phoneNode(string name, string phonrNumber, string address, phoneNode* next)
-{
-    name_ = name;
-    phoneNumber_ = phonrNumber;
-    address_ = address;
-    next_ = next;
-};
-bool phoneBook::Insert(string name, string phonrNumber, string address)
-{
-    phoneNode* ptr = lst_;
-    if (ptr == NULL)
-    {
-        lst_ = new phoneNode(name, phonrNumber, address, 0);
-        return 1;
-    }
-    else
-    {
-        while (ptr->next_ != NULL)
-        {
-            ptr = ptr->next_;
-        }
-        ptr->next_ = new phoneNode(name, phonrNumber, address, 0);
-    }
-
-    return 1;
-}
-void phoneBook::Delete(string name)
-{
-    phoneNode* ptr = lst_;
-    phoneNode* dptr;
-    if (ptr->next_ == NULL && ptr->next_->name_ == name)
-    {
-        delete ptr;
-        return;
-    }
-    while (ptr->next_ != NULL) {
-        if (ptr->next_->name_ == name)
-        {
-            dptr = ptr->next_;
-            ptr = ptr->next_->next_;
-            delete dptr;
-            return;
-        }
-    }
-
-}
-phoneNode* phoneBook::Find(string name)
-{
-    phoneNode* ptr = lst_;
-
-    if (ptr->next_ == NULL)
-    {
-        return ptr;
-    }
-    while (ptr->next_ != NULL)
-    {
-        if (ptr->name_ == name)
-            return ptr;
-        ptr = ptr->next_;
-    }
-    return ptr;
-}
-void phoneNode::printPD() {
-    cout << "name : " << name_ << " ";
-    cout << "phoneNumber : " << phoneNumber_ << " ";
-    cout << "address : " << address_ << " ";
-}
-int main() {
-    phoneBook* pb = new phoneBook();
-    pb->Insert("누구나", "010-2345-6789", "동의대학교");
-    pb->Insert("아무개", "010-9876-5432", "동의대학교");
-
-    phoneNode* pn;
-    pn = pb->Find("누구나");
-    if (pn) pn->printPD();
-
-    pn = pb->Find("아무개");
-    if (pn) pn->printPD();
-
-    pb->Delete("아무개");
-
-    pn = pb->Find("누구나");
-    if (pn) pn->printPD();
-    pn = pb->Find("아무개");
-    if (pn) pn->printPD();
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class phoneNode {
+//public:
+//    phoneNode(string name, string phonrNumber, string address, phoneNode* next = 0);
+//    void printPD();
+//    phoneNode* next_;
+//    string name_;
+//    string phoneNumber_;
+//    string address_;
+//};
+//
+//class phoneBook {
+//public:
+//    phoneBook();
+//    bool Insert(string name, string phonrNumber, string address);
+//    void Delete(string name);
+//    phoneNode* Find(string name);
+//private:
+//    phoneNode* lst_;
+//};
+//phoneBook::phoneBook()
+//{
+//    phoneNode* lst_ = NULL;
+//};
+//phoneNode::phoneNode(string name, string phonrNumber, string address, phoneNode* next)
+//{
+//    name_ = name;
+//    phoneNumber_ = phonrNumber;
+//    address_ = address;
+//    next_ = next;
+//};
+//bool phoneBook::Insert(string name, string phonrNumber, string address)
+//{
+//    phoneNode* ptr = lst_;
+//    if (ptr == NULL)
+//    {
+//        lst_ = new phoneNode(name, phonrNumber, address, 0);
+//        return 1;
+//    }
+//    else
+//    {
+//        while (ptr->next_ != NULL)
+//        {
+//            ptr = ptr->next_;
+//        }
+//        ptr->next_ = new phoneNode(name, phonrNumber, address, 0);
+//    }
+//
+//    return 1;
+//}
+//void phoneBook::Delete(string name)
+//{
+//    phoneNode* ptr = lst_;
+//    phoneNode* dptr;
+//    if (ptr->next_ == NULL && ptr->next_->name_ == name)
+//    {
+//        delete ptr;
+//        return;
+//    }
+//    while (ptr->next_ != NULL) {
+//        if (ptr->next_->name_ == name)
+//        {
+//            dptr = ptr->next_;
+//            ptr = ptr->next_->next_;
+//            delete dptr;
+//            return;
+//        }
+//    }
+//
+//}
+//phoneNode* phoneBook::Find(string name)
+//{
+//    phoneNode* ptr = lst_;
+//
+//    if (ptr->next_ == NULL)
+//    {
+//        return ptr;
+//    }
+//    while (ptr->next_ != NULL)
+//    {
+//        if (ptr->name_ == name)
+//            return ptr;
+//        ptr = ptr->next_;
+//    }
+//    return ptr;
+//}
+//void phoneNode::printPD() {
+//    cout << "name : " << name_ << " ";
+//    cout << "phoneNumber : " << phoneNumber_ << " ";
+//    cout << "address : " << address_ << " ";
+//}
+//int main() {
+//    phoneBook* pb = new phoneBook();
+//    pb->Insert("누구나", "010-2345-6789", "동의대학교");
+//    pb->Insert("아무개", "010-9876-5432", "동의대학교");
+//
+//    phoneNode* pn;
+//    pn = pb->Find("누구나");
+//    if (pn) pn->printPD();
+//
+//    pn = pb->Find("아무개");
+//    if (pn) pn->printPD();
+//
+//    pb->Delete("아무개");
+//
+//    pn = pb->Find("누구나");
+//    if (pn) pn->printPD();
+//    pn = pb->Find("아무개");
+//    if (pn) pn->printPD();
+//}
